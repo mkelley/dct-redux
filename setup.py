0e07a9a57fd68a1858cbd37a46724aededd276aa
Michael S. P. Kelley <msk@astro.umd.edu>
2017-07-12 15:07:01 -0400

    New setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from glob import glob
+from distutils.core import setup
+
+setup(name='dct-redux',
+      version='0.1',
+      description='DCT/LMI reduction and analysis scripts.',
+      author='Michael S. P. Kelley',
+      author_email='msk@astro.umd.edu',
+      url='https://github.com/mkelley/dct-redux',
+      scripts=glob('bin/*.py')
+     )
