b4c022f0119aa63942902435707c1de278901d30
Michael S. P. Kelley <msk@astro.umd.edu>
2022-08-31 09:20:06 -0400

    v0.7.2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# dct-redux v0.7.1
+# dct-redux v0.7.2
 Python scripts for reducing Lowell Discovery Telescope (formerly Discovery Channel Telescope) data, mostly focused on observations of comets.
 
 ## Status
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="dct-redux",
-    version="0.7.1",
+    version="0.7.2",
     description="DCT/LMI reduction and analysis scripts.",
     author="Michael S. P. Kelley",
     author_email="msk@astro.umd.edu",
